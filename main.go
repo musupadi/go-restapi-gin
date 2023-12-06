8a1444efb0138ad10e001abac8bdbfb170b054b0
Muhammad Supriyadi <33887565+musupadi@users.noreply.github.com>
2023-12-06 14:24:51 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/tentangkode/go-restapi-gin/controllers/productcontroller"
-	"github.com/tentangkode/go-restapi-gin/models"
+	"github.com/musupadi/go-restapi-gin/controllers/productcontroller"
+	"github.com/musupadi/go-restapi-gin/models"
 
 	"github.com/gin-gonic/gin"
 )
